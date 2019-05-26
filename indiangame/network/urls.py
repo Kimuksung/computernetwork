7d46dc66619d5f00b1f4771e07bfa1bf542e358f
kimuksung <kimuksung2@daum.net>
2019-05-26 18:21:36 +0900

    playerid/roomlist

--- indiangame/network/urls.py
+++ indiangame/network/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.addid, name='addid'),
+    path('register', views.register, name='register'),
+    path('roomlist', views.roomlist, name='roomlist'),
 ]
--- indiangame/network/views.py
+++ indiangame/network/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Player,Room
 # Create your views here.
 def addid(request):
     return render(request, 'network/ID.html', {})
+
+def register(request):
+    try:
+        nickname=request.POST['nickname']
+        nickname=nickname.strip()
+        if nickname == "":
+            raise NameError #ID를 설정 안했을 경우
+        new_player=Player(nickname=nickname)
+        new_player.save()
+        #return HttpResponse("완료")
+    except NameError:
+        return HttpResponse("<script>alert('닉네임을 입력하지 않았습니다. 다시 입력해주세요.'); history.go(-1);</script>");
+    except:
+        return HttpResponse("<script>alert('아이디 중복입니다.'); history.go(-1);</script>");
+    return render(request, 'network/introdution.html', {})
+
+def roomlist(request):
+    rooms = Room.objects.all()
+    #print(rooms)
+    return render(request, 'network/room_list.html', {'rooms':rooms})
