fdd70af29af48e872872a15418213ccb6184dff3
kimuksung <kimuksung2@daum.net>
2019-05-26 18:22:09 +0900

    Revert "playerid/roomlist"
    This reverts commit 7d46dc66619d5f00b1f4771e07bfa1bf542e358f.

--- indiangame/network/urls.py
+++ indiangame/network/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.addid, name='addid'),
-    path('register', views.register, name='register'),
-    path('roomlist', views.roomlist, name='roomlist'),
 ]
